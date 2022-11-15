2afa7db058d76e261b715007e4d159d94412f62a
Gradi0us <hieunguyen20032302@gmail.com>
2022-11-15 22:40:52 +0700

    hiu push code

---
+++ app/src/main/java/com/example/test_duan1_ui/Search_Main.java
@@ -0,0 +1,23 @@
+package com.example.test_duan1_ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Search_Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_main);
+        findViewById(R.id.tomainsrc).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Search_Main.this,Main_screen.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
